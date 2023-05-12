c9439e2d27ea3510932f7b348b3647845bcb7930
jaibwtl <128355134+jaibwtl@users.noreply.github.com>
2023-05-12 09:13:57 +0530

    Create splunk-install-docker.sh

---
+++ splunk-install-docker.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y 
+sudo apt-get install     ca-certificates     curl     gnupg -y sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+sudo docker run  -p 8000:8000 -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=Alpha#05082023" -d --name splunk splunk/splunk:latest
+sudo docker ps
+
+#URL: http://PublicIP:8000 eg. http://18.234.150.236:8000/
+#User: admin
+#Password: Alpha#05082023
